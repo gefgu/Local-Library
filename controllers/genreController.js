316633078573832a25b02444999dc0f288604c9f
gefgu <gefgu@hotmail.com>
2022-07-04 16:20:08 -0300

    Add display of genres list

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,7 +1,18 @@
 const Genre = require("../models/genre");
 
-exports.genre_list = (req, res) => {
-  res.send("NOT IMPLEMENTED: genre list");
+exports.genre_list = (req, res, next) => {
+  Genre.find()
+    .sort([["name", "ascending"]])
+    .exec(function (err, list_genres) {
+      if (err) {
+        return next(err);
+      }
+
+      res.render("genre_list", {
+        title: "Genre List",
+        genre_list: list_genres,
+      });
+    });
 };
 
 exports.genre_detail = (req, res) => {
