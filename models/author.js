d9c23a4d7fe22ffde39879295822f73d647d71a5
gefgu <gefgu@hotmail.com>
2022-07-04 16:16:41 -0300

    Add display of authors list

--- models/author.js
+++ models/author.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
 
 const AuthorSchema = new Schema({
   first_name: { type: String, required: true, maxLength: 100 },
@@ -21,7 +22,7 @@ AuthorSchema.virtual("name").get(function () {
   return fullName;
 });
 
-AuthorSchema.virtual("lifespan").get(function ()  {
+AuthorSchema.virtual("lifespan").get(function () {
   let lifetime_string = "";
   if (this.date_of_birth) {
     lifetime_string = this.date_of_birth.getYear().toString();
@@ -33,8 +34,20 @@ AuthorSchema.virtual("lifespan").get(function ()  {
   return lifetime_string;
 });
 
-AuthorSchema.virtual("url").get(function ()  {
+AuthorSchema.virtual("url").get(function () {
   return `/catalog/author/${this._id}`;
+});
+
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+  return this.date_of_birth
+    ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+    : "";
+});
+
+AuthorSchema.virtual("date_of_death_formatted").get(function () {
+  return this.date_of_death
+    ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+    : "";
 });
 
 module.exports = mongoose.model("Author", AuthorSchema);
